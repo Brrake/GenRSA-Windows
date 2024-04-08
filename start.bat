9258465ebce94f6170af4e8196e3f164831c5eaa
Davide Rubiu <daviderubiu5@gmail.com>
2024-04-08 22:08:56 +0200

    Minor fixs

--- start.bat
+++ start.bat
@@ -1,13 +1,12 @@
 @echo off
 
-echo Benvenuto su GenRSA-Windows!
+echo ---- Benvenuto su GenRSA-Windows! ----
 
 rem Controlliamo se il file variables.bat esiste
 if not exist resources\variables.bat (
-    echo Errore: Il file variables.bat non è stato trovato.
-    echo Assicurati di avere tutti i file necessari e riprova.
+    echo ---- Errore: Il file variables.bat non è stato trovato. ----
+    echo ---- Assicurati di avere tutti i file necessari e riprova. ----
     pause
-
     exit /b
 )
 
@@ -15,25 +14,25 @@ call resources/variables.bat
 
 rem Controlliamo se Git è installato nel percorso specificato
 if not exist %GIT_PATH% (
-    echo Errore: Non e' stato possibile trovare Git nel percorso specificato: %GIT_PATH%
-    echo Assicurati di aver installato Git e di aver configurato correttamente il percorso nel file resources\variables.bat.
+    echo ---- Errore: Non e' stato possibile trovare Git nel percorso specificato: %GIT_PATH% ----
+    echo ---- Assicurati di aver installato Git e di aver configurato correttamente il percorso nel file resources\variables.bat. ----
+    echo ---- Scarica Git da questo link : https://git-scm.com/download/win ----
     pause
-    
     exit /b
 )
 
-rem Check if the keys directory exists, if not, create it
+rem Verifica che la cartella di out esista
 if not exist %OPENSSL_OUT_DIR% (
-    echo Creazione cartella di out...
+    echo ---- Creazione cartella di out... ----
     mkdir %OPENSSL_OUT_DIR%
 )
 
-echo Generazione...
-
 rem Generate private key
+echo ---- Generazione private key... ----
 %OPENSSL_GIT_PATH% genpkey -algorithm RSA -out %OPENSSL_OUT_DIR%\private_key.pem -pkeyopt rsa_keygen_bits:%OPENSSL_KEY_SIZE%
 
 rem Generate public key from private key
+echo ---- Generazione public key... ----
 %OPENSSL_GIT_PATH% rsa -pubout -in %OPENSSL_OUT_DIR%\private_key.pem -out %OPENSSL_OUT_DIR%\public_key.pem
 
 pause
