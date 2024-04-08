f4c64af62138ee48956e2719c079830bdfc8ca1e
Davide Rubiu <daviderubiu5@gmail.com>
2024-04-08 22:19:36 +0200

    Minor fixs

--- start.bat
+++ start.bat
@@ -2,7 +2,7 @@
 
 echo ---- Benvenuto su GenRSA-Windows! ----
 
-rem Controlliamo se il file variables.bat esiste
+rem Controlla se il file variables.bat esiste
 if not exist resources\variables.bat (
     echo ---- Errore: Il file variables.bat non è stato trovato. ----
     echo ---- Assicurati di avere tutti i file necessari e riprova. ----
@@ -12,7 +12,7 @@ if not exist resources\variables.bat (
 
 call resources/variables.bat
 
-rem Controlliamo se Git è installato nel percorso specificato
+rem Controlla se Git è installato nel percorso specificato
 if not exist %GIT_PATH% (
     echo ---- Errore: Non e' stato possibile trovare Git nel percorso specificato: %GIT_PATH% ----
     echo ---- Assicurati di aver installato Git e di aver configurato correttamente il percorso nel file resources\variables.bat. ----
@@ -27,11 +27,11 @@ if not exist %OPENSSL_OUT_DIR% (
     mkdir %OPENSSL_OUT_DIR%
 )
 
-rem Generate private key
+rem Genera private key
 echo ---- Generazione private key... ----
 %OPENSSL_GIT_PATH% genpkey -algorithm RSA -out %OPENSSL_OUT_DIR%\private_key.pem -pkeyopt rsa_keygen_bits:%OPENSSL_KEY_SIZE%
 
-rem Generate public key from private key
+rem Genera public key da private key
 echo ---- Generazione public key... ----
 %OPENSSL_GIT_PATH% rsa -pubout -in %OPENSSL_OUT_DIR%\private_key.pem -out %OPENSSL_OUT_DIR%\public_key.pem
 
