22e4a9509d210f4f54b895d2619e0f106418f566
Davide Rubiu <daviderubiu5@gmail.com>
2024-04-08 19:55:02 +0200

    Minor fixs

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # GenRSA-Windows
 Generate .PEM keys using git cli
 
-1) Download git from https://git-scm.com/download/win cli before start the start.bat script.
+1) Scarica GIT dal link : https://git-scm.com/download/win 
 
-2) Open start.bat script 
+2) Modifica il file "start.bat" assicurandoti che GIT_PATH corrisponda al path della cartella bin dell'installazione effettuata da GIT
+
+3) Apri il file "start.bat" 
--- start.bat
+++ start.bat
@@ -1,3 +1,6 @@
-"C:\Program Files\Git\usr\bin\openssl" genpkey -algorithm RSA -out keys/private_key.pem -pkeyopt rsa_keygen_bits:2048
-"C:\Program Files\Git\usr\bin\openssl" rsa -pubout -in keys/private_key.pem -out keys/public_key.pem
+@echo off
+set GIT_PATH="C:\Program Files\Git\usr\bin"
+set OPENSSL_GIT_PATH="%GIT_PATH%\openssl"
+%OPENSSL_GIT_PATH% genpkey -algorithm RSA -out keys\private_key.pem -pkeyopt rsa_keygen_bits:2048
+%OPENSSL_GIT_PATH% rsa -pubout -in keys\private_key.pem -out keys\public_key.pem
 pause
