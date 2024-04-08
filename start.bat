3c19ee9b49d8037e5e4246fdb4fcc566c4486331
Davide Rubiu <daviderubiu5@gmail.com>
2024-04-08 21:51:04 +0200

    Minor fixs

--- start.bat
+++ start.bat
@@ -1,16 +1,20 @@
 @echo off
 set GIT_PATH="C:\Program Files\Git\usr\bin"
+
+rem OpenSSL Configuration
 set OPENSSL_GIT_PATH=%GIT_PATH%\openssl
+set OPENSSL_KEY_SIZE=2048
+set OPENSSL_OUT_DIR=keys
 
 rem Check if the keys directory exists, if not, create it
-if not exist keys (
-    mkdir keys
+if not exist %OPENSSL_OUT_DIR% (
+    mkdir %OPENSSL_OUT_DIR%
 )
 
 rem Generate private key
-%OPENSSL_GIT_PATH% genpkey -algorithm RSA -out keys\private_key.pem -pkeyopt rsa_keygen_bits:2048
+%OPENSSL_GIT_PATH% genpkey -algorithm RSA -out %OPENSSL_OUT_DIR%\private_key.pem -pkeyopt rsa_keygen_bits:%OPENSSL_KEY_SIZE%
 
 rem Generate public key from private key
-%OPENSSL_GIT_PATH% rsa -pubout -in keys\private_key.pem -out keys\public_key.pem
+%OPENSSL_GIT_PATH% rsa -pubout -in %OPENSSL_OUT_DIR%\private_key.pem -out %OPENSSL_OUT_DIR%\public_key.pem
 
 pause
