e77c4f5f08dbc22c77b5c23c0a06644812dea0e0
Davide Rubiu <rubiu@dauvea.it>
2024-06-03 11:15:14 +0200

    Minor fix

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Genera chiavi RSA utilizzando openssl.exe
 
     - OPENSSL_KEY_SIZE : Dimensione in bit della chiave RSA -- DEFAULT : 2048
     - OPENSSL_OUT_DIR : Path di uscita della chiavi (Pubblica/Privata) -- DEFAULT : ./keys
+    - OPENSSL_PATH : Path openssl.exe -- DEFAULT :"C:\Program Files\Git\usr\bin"
 
 Fonti : 
     - openssl.exe -> Git : https://git-scm.com/download/win 
--- start.bat
+++ start.bat
@@ -1,17 +1,49 @@
 @echo off
+:: BatchGotAdmin
+::-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"="
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+::--------------------------------------
+
+
 
 set OPENSSL_KEY_SIZE=2048
-set OPENSSL_OUT_DIR=keys
+set PROJ_DIR=%cd%
+set OPENSSL_OUT_DIR=%PROJ_DIR%/keys
+
+set OPENSSL_PATH="C:\Program Files\Git\usr\bin"
+
+cd %OPENSSL_PATH%
 
 echo ---- Benvenuto su GenRSA-Windows! ----
 
 rem Controlla se il file variables.bat esiste
-if not exist resources\openssl.exe (
-    echo ---- Errore: Il file openssl.exe non è stato trovato. ----
+if not exist .\openssl.exe (
+    echo ---- Errore: Il file openssl.exe non e' stato trovato. ----
     echo ---- Assicurati di avere tutti i file necessari e riprova. ----
     pause
     exit /b
 )
+
 
 rem Verifica che la cartella di out esista
 if not exist %OPENSSL_OUT_DIR% (
@@ -21,10 +53,10 @@ if not exist %OPENSSL_OUT_DIR% (
 
 rem Genera private key
 echo ---- Generazione private key... ----
-.\resources\openssl genpkey -algorithm RSA -out %OPENSSL_OUT_DIR%\private_key.pem -pkeyopt rsa_keygen_bits:%OPENSSL_KEY_SIZE%
+.\openssl genpkey -algorithm RSA -out %OPENSSL_OUT_DIR%\private_key.pem -pkeyopt rsa_keygen_bits:%OPENSSL_KEY_SIZE%
 
 rem Genera public key da private key
 echo ---- Generazione public key... ----
-.\resources\openssl rsa -pubout -in %OPENSSL_OUT_DIR%\private_key.pem -out %OPENSSL_OUT_DIR%\public_key.pem
+.\openssl rsa -pubout -in %OPENSSL_OUT_DIR%\private_key.pem -out %OPENSSL_OUT_DIR%\public_key.pem
 
 pause
